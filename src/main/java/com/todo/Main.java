583fa4ae9a4c0c99c9370f3f3681d081c4717f16
dhinakaran311 <kit27.am16@gmail.com>
2025-09-16 13:57:54 +0530

    db connected

--- src/main/java/com/todo/Main.java
+++ src/main/java/com/todo/Main.java
@@ -1,7 +1,18 @@
 package com.todo;
 
+import com.todo.util.DatabaseUtil;
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, ToDo Application!");
+        DatabaseUtil db_Connection = new DatabaseUtil();
+        try {
+            Connection cn = db_Connection.getDBConnection();
+            if (cn != null) {
+                System.out.println("✅Database Connection Successful");
+            }
+        } catch (Exception e) {
+            System.out.println("❌❌Database Connection Failed: " + e.getMessage());
+        }
     }
 }
---
+++ src/main/java/com/todo/util/DatabaseUtil.java
@@ -0,0 +1,38 @@
+package com.todo.util;
+
+import java.sql.Connection; 
+import java.sql.DriverManager; //JDBC API
+import java.sql.SQLException; 
+
+public class DatabaseUtil {
+
+    // Update these values to match your MySQL setup
+    private static final String URL = "jdbc:mysql://localhost:3306/todo?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";      // replace with your MySQL username
+    private static final String PASSWORD = "Dhina@311";  // replace with your MySQL password
+
+    static{
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch(ClassNotFoundException e){
+            System.out.println("MySQL JDBC Driver not found.");
+        }
+    }
+    // Get a database connection
+    public Connection getDBConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    // // Optional: close connection safely
+    // public static void closeConnection(Connection conn) {
+    //     if (conn != null) {
+    //         try {
+    //             conn.close();
+    //         } catch (SQLException e) {
+    //             e.printStackTrace();
+    //         }
+    //     }
+    // }
+
+}
